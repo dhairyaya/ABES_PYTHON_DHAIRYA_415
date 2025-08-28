dcf89e91424da367bf97d59bc1d13cbcdc7b0486
Dhairya Pratap Solanki <pratapdhairya67@gmail.com>
2025-08-28 18:31:36 +0530

    String operations

---
+++ datatype.py
@@ -0,0 +1,27 @@
+var1= 1
+var2=True
+var3=10.023
+var4=10+3j
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+
+#single line string('' and " ")
+a="Hello"
+b="Friend"
+print(a,b)
+
+#Multiline string(''' ''')
+c='''If you want, I can:
+
+Draw a small table of shapes and results, or
+
+Show an animated step (pseudo) of broadcasting alignment, or
+
+Give a few exercises you can paste into the REPL to practice.'''
+print(c)
+
+
+#String length
+a="Hello World"
